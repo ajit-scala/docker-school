3b1c9d056e09196cd77f269ea6359eb538d09d33
Ajit Chahal <achahal@autoscout24.com>
2017-03-14 15:31:20 +0100

    refactor template, provide place holders for port and location path

--- docker-reverse-proxy/rebuild.sh
+++ docker-reverse-proxy/rebuild.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+LOCAL_LOCATION="oidc"
+LOCAL_PORT="9000"
+
 MYIP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep '10.0')"
 echo $MYIP
-cat nginx.conf.template | sed s/IP_ADDRESS/$MYIP/ > nginx.conf
+cat nginx.conf.template | sed s/IP_ADDRESS/$MYIP/ | sed s/LOCAL_LOCATION/$LOCAL_LOCATION/ | sed s/LOCAL_PORT/$LOCAL_PORT/ > nginx.conf
 
 docker stop nginx-rev
 docker rm nginx-rev
